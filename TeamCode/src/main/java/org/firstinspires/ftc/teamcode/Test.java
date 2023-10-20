fda6d82d912bcb6f0e91e53af79e6e25eb2ee0a2
tg-32608 <125822330+tg-32608@users.noreply.github.com>
2023-10-20 17:31:35 -0500

    hh

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+
+// TODO: remove Actions from the core module?
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.firstinspires.ftc.teamcode.TankDrive;
+import org.firstinspires.ftc.teamcode.tuning.TuningOpModes;
+
+@Autonomous(name="1115")
+public final class Test extends LinearOpMode {
+    public static double DISTANCE = 64;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        if (TuningOpModes.DRIVE_CLASS.equals(MecanumDrive.class)) {
+            MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
+
+            waitForStart();
+                Actions.runBlocking(
+                        drive.actionBuilder(drive.pose)
+                                .splineTo(new Vector2d(15, 0), 0)
+                                .build());
+        } else {
+            throw new AssertionError();
+        }
+    }
+}
