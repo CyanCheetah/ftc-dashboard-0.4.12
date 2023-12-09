f53a7d1ea72397f78c158d3988b6e6a5122f6ca8
CyanCheetah <saitanuj12@gmail.com>
2023-12-09 14:21:17 -0600

    CyanChangedConstants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorConstantValues.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorConstantValues.java
@@ -5,11 +5,13 @@ package org.firstinspires.ftc.teamcode;
  */
 public class MotorConstantValues {
     private double SwingOutPosition = 0;
+
     private double SwingScorePosition = 0.275;
+
     private double SwingInPosition = 0.31;
     private double BucketOutPosition = .16;
-    private double BucketInPosition = BucketOutPosition + .155;
 
+    private double BucketInPosition = BucketOutPosition + .155;
     private double clawClose = .95;
     private double clawSemiOpen = .9;
     private double clawFullOpen = .75;
@@ -26,16 +28,16 @@ public class MotorConstantValues {
         return SwingOutPosition;
     }
     public double getSwingScorePosition() {
-        return SwingOutPosition;
+        return SwingScorePosition;
     }
     public double getSwingInPosition() {
-        return SwingOutPosition;
+        return SwingInPosition;
     }
     public double getBucketOutPosition() {
-        return SwingOutPosition;
+        return BucketOutPosition;
     }
     public double getBucketInPosition() {
-        return SwingOutPosition;
+        return BucketInPosition;
     }
 
 }
